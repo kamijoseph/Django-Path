015519bb2836b26374a919c673e3408244ded96b
Kami Joseph <josephkiarie561@gmail.com>
2024-12-10 15:51:58 +0300

    0.0.3

--- Project_1-/blog/posts/urls.py
+++ Project_1-/blog/posts/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
-from . import urls
+from . import views
 
 urlpatterns = [
-    path('posts/helloworld/', views.helloworld)
+    path('helloworld/', views.helloworld),
 ]
